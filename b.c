86cba0f4c51e241bbca7f1ecf374071972729311
JEGANMANIKANDAN <35652628+JEGANMANIKANDAN@users.noreply.github.com>
2018-02-13 16:45:35 +0530

    Create b.c

---
+++ b.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,d,sum=0,i;
+clrscr();
+printf("Enter the a and b and c value:");
+scanf("%d%d%d",&a,&b,&,c);
+
+for(i=1;i<=d;i++)
+{
+d=a+(1-i)*c;
+sum=sum+c;
+}
+printf("\n %d",sum);
+
+getch();
+}
